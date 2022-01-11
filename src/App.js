b77309d7591c32f75dc87b9c2dbca8513b02b366
Naveen <78008387+Naveen2004@users.noreply.github.com>
2022-01-11 19:04:02 +0530

    Basic UI done and backend support needed, AVLODHAN..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Header, Main} from './components';
+
+
+const sampleData = {
+    question: "What is your name?",
+    options: [
+        "Naveen",
+        "SK",
+        "SK Naveen",
+        "Naveen SK"
+    ]
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="d-flex flex-column h-100">
+        <Header/>
+        <Main data={sampleData}/>
+      </div>
   );
 }
 
